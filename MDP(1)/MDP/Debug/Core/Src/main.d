c262f2ea3bba1a9ea0207aad77576809d1d51d34
J <71466939+Jseow008@users.noreply.github.com>
2024-02-23 11:04:09 +0800

    edits

--- MDP(1)/MDP/Debug/Core/Src/main.d
+++ MDP(1)/MDP/Debug/Core/Src/main.d
@@ -38,7 +38,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
- C:/Users/Javier/Desktop/MDP(1)/MDP/PeripheralDriver/Inc/oled.h
+ C:/Users/Javier/Desktop/GITMDP/MDP/MDP(1)/MDP/PeripheralDriver/Inc/oled.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -79,4 +79,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
-C:/Users/Javier/Desktop/MDP(1)/MDP/PeripheralDriver/Inc/oled.h:
+C:/Users/Javier/Desktop/GITMDP/MDP/MDP(1)/MDP/PeripheralDriver/Inc/oled.h:
